69963ef55e4b3a8eb5cd98e6a210b9e7cd217a31
daswahr <daswahr@gmail.com>
2017-02-22 10:49:00 +0300

    create even.js with logic and start step 5

---
+++ src/even.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+import readlineSync from 'readline-sync';
+
+const random = () => {
+  return Math.floor(Math.random() * 100) + 1;;
+};
+
+const brainEven = (name) => {
+  let i = 0;
+  while (i !== 3) {
+    const num = random();
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('Your answer: ', {
+      trueValue: ['yes'],
+      falseValue: ['no'],
+    });
+    
+    if (((num % 2 === 0) && (answer === true)) || ((num % 2 != 0) && (answer === false))) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+
+      const correctAnswer = (num) => {
+        if (num % 2 === 0) {
+          return 'yes';
+          }
+        return 'no';
+      };
+      
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer(num)}'`);
+      console.log(`Let's try again, ${name}`);
+    }
+  }
+};
+
+export default () => {
+  console.log('Welcome to the Brain Games!\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`\nHello, ${name}!\n`);
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  brainEven(name);
+  console.log(`Congratulations, ${name}!`);
+};
