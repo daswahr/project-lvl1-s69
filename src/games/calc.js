63c377de240007b0e4f4812b07d08801d7275546
daswahr <daswahr@gmail.com>
2017-02-24 20:23:36 +0300

    upgrade

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,8 +1,6 @@
 import game from '../';
 
-const random = () => {
-  return Math.floor(Math.random() * 100) + 1;
-};
+const random = () => (Math.floor(Math.random() * 100) + 1);
 
 const task = 'What is the result of the expression?';
 
